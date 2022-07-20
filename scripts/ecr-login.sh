efb3a4ab3f4dac5971f81ea7ea49b48bc8d580bb
Rajan Khullar <rkhullar03@gmail.com>
2022-07-20 17:26:20 -0400

    save

--- scripts/ecr-login.sh
+++ scripts/ecr-login.sh
@@ -5,6 +5,7 @@
 ECR_CREDS_ACCESS_KEY=${ECR_CREDS_ACCESS_KEY:-ecr_access_key}
 ECR_CREDS_SECRET_KEY=${ECR_CREDS_SECRET_KEY:-ecr_secret_key}
 
+export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
 AWS_ACCESS_KEY_ID=$(kubectl get secret "$ECR_CREDS" -o jsonpath="{.data.${ECR_CREDS_ACCESS_KEY}}" | base64 -d)
 AWS_SECRET_ACCESS_KEY=$(kubectl get secret "$ECR_CREDS" -o jsonpath="{.data.${ECR_CREDS_SECRET_KEY}}" | base64 -d)
 
